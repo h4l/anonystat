90309bee8e8253e435ed9cced74abeec03beb393
Hal Blackburn <hwtb2@cam.ac.uk>
2024-05-05 17:10:49 +0000

    feat: implement fractional month/date difference
    We now use fractional numbers to represent the difference between dates
    in a given unit. e.g. 0.4 months rather than 0 or 1. This allows the
    caller to round the value in a way they need, without adding a bunch of
    rounding options to the API and trying to accomodate all use cases.

--- _datetime.ts
+++ _datetime.ts
@@ -1,6 +1,9 @@
 import { assertUnreachable } from "./_misc.ts";
 
 export type Unit =
+  | "milliseconds"
+  | "seconds"
+  | "minutes"
   | "hours"
   | "days"
   | "weeks"
@@ -8,15 +11,19 @@ export type Unit =
   | "quarters"
   | "years";
 
-const SECOND = 1000,
+export const SECOND = 1000,
   MINUTE = SECOND * 60,
   HOUR = MINUTE * 60,
   DAY = HOUR * 24,
   WEEK = DAY * 7;
 
-export function getUtcTimeSinceMidnight(date: Date): number {
-  return date.getUTCHours() * HOUR + date.getUTCMinutes() * MINUTE +
-    date.getUTCSeconds() * SECOND + date.getUTCMilliseconds();
+/** The number of milliseconds since midnight in UTC.
+ *
+ * i.e. the time contribution to the date's timestamp.
+ */
+export function timeSinceMidnightUtc(date: Date): number {
+  return date.getTime() -
+    Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
 }
 
 // Deno std lib has a difference() function that almost does what we need,
@@ -25,40 +32,127 @@ export function getUtcTimeSinceMidnight(date: Date): number {
 // midnight. We only need/support single-unit periods, so we only need to
 // calculate difference for one unit.
 
-/** Get the difference between two dates in the given time unit.
+/** Get the fractional difference between dates two dates in the given `unit`.
  *
- * @returns The integer number of units difference, negative if to < from.
+ * When `to` is before `from`, the difference is negative. When the dates are
+ * exact multiples of `unit` apart, the result is an exact integer, otherwise
+ * it's a fractional value.
+ *
+ * The weeks, days and hours units are linear time units as you'd expect. The
+ * month and up units are a little unusual in that months vary in length, so the
+ * fractions vary according to the month(s) they bridge.
+ *
+ * Month fractions work by placing the `from` day & time in each month, and
+ * finding the relative position of `to` (in linear time) between the two
+ * surrounding points.
  */
-export function utcDifference(
-  { from, to, unit }: { from: Date; to: Date; unit: Unit },
-): number {
+export function differenceUtc(from: Date, to: Date, unit: Unit): number {
   switch (unit) {
+    case "milliseconds":
+      return (to.getTime() - from.getTime());
+    case "seconds":
+      return (to.getTime() - from.getTime()) / SECOND;
+    case "minutes":
+      return (to.getTime() - from.getTime()) / MINUTE;
     case "hours":
-      return Math.trunc((to.getTime() - from.getTime()) / HOUR);
+      return (to.getTime() - from.getTime()) / HOUR;
     case "days":
-      return Math.trunc((to.getTime() - from.getTime()) / DAY);
+      return (to.getTime() - from.getTime()) / DAY;
     case "weeks":
-      return Math.trunc((to.getTime() - from.getTime()) / WEEK);
+      return (to.getTime() - from.getTime()) / WEEK;
     case "months":
-      return getUtcMonthDifference(from, to);
+      return monthDifferenceUtc(from, to);
     case "quarters":
-      return Math.trunc(getUtcMonthDifference(from, to) / 3);
+      return monthDifferenceUtc(from, to) / 3;
     case "years":
-      return Math.trunc(getUtcMonthDifference(from, to) / 12);
+      return monthDifferenceUtc(from, to) / 12;
   }
   assertUnreachable(unit);
 }
 
-function getUtcMonthDifference(from: Date, to: Date): number {
-  let monthDelta = (to.getUTCFullYear() - from.getUTCFullYear()) * 12;
-  monthDelta += to.getUTCMonth() - from.getUTCMonth();
-  if (monthDelta === 0) return 0;
-  // If `to` has not passed `from` within the month, not a full month has passed
-  let delta = to.getUTCDate() - from.getUTCDate();
-  if (delta === 0) { // same day, so find the exact difference in milliseconds
-    delta = getUtcTimeSinceMidnight(to) - getUtcTimeSinceMidnight(from);
+/**
+ * Get the fractional number of months between from and to.
+ *
+ * The value is negative when `to` is before `from`. The fractional component is
+ * the relative position of `to` between the `from` month boundaries either side
+ * of it, with millisecond precision.
+ */
+export function monthDifferenceUtc(from: Date, to: Date): number {
+  const toY = to.getUTCFullYear(),
+    toM = to.getUTCMonth(),
+    fromY = from.getUTCFullYear(),
+    fromM = from.getUTCMonth(),
+    fromD = from.getUTCDate(),
+    toTime = to.getTime(),
+    fromTimeInDay = timeSinceMidnightUtc(from);
+
+  const wholeMonths = (toY - fromY) * 12 + (toM - fromM);
+
+  // We calculate fractional months by finding two points on either side of `to`
+  // at the same day/time in the month as `from`. We then calculate a 0..1 value
+  // representing the position of `to` between the two boundaries.
+
+  // If the from day does not occur in a month, the boundary will be in the
+  // month above. In this case we treat the boundary as occurring in imaginary
+  // space after the end of the lower month and before the start of the upper
+  // month.
+  //
+  // For example, from 2024-01-31 counting forward, Feb 29th is the last day
+  // before March 1st. Clearly a month has not passed by Feb 29th, but has by
+  // March 1st.  [02-01, 02-29][...02-31...][03-01, 03-31]
+  //                             boundary
+
+  // Get the `from` time within 1 month of `to` to use as one of the boundaries.
+  const boundary = Date.UTC(toY, toM, fromD) + fromTimeInDay;
+
+  let lowerBoundary: number, upperBoundary: number;
+
+  if (boundary <= toTime) {
+    lowerBoundary = boundary;
+    upperBoundary = clampImaginaryDays(
+      Date.UTC(toY, toM + 1, fromD) + fromTimeInDay,
+      toY,
+      toM + 1,
+      "up",
+    );
+  } else {
+    upperBoundary = clampImaginaryDays(boundary, toY, toM, "up");
+    lowerBoundary = clampImaginaryDays(
+      Date.UTC(toY, toM - 1, fromD) + fromTimeInDay,
+      toY,
+      toM - 1,
+      "down",
+    );
   }
-  if (monthDelta < 0 && delta > 0) return monthDelta + 1;
-  if (monthDelta > 0 && delta < 0) return monthDelta - 1;
-  return monthDelta;
+
+  const monthLength = upperBoundary - lowerBoundary;
+  let relativePosition = (toTime - lowerBoundary) / monthLength;
+
+  // If the boundary (which wholeMonths is based on) is after the toTime, we've
+  // completed less than `wholeMonths`, so we're subtracting the relative
+  // position rather than adding it, so we invert it.
+  if (boundary > toTime) relativePosition = -(1 - relativePosition);
+
+  return wholeMonths + relativePosition;
+}
+
+/** Constrain a timestamp to not exceed the upper end of a month.
+ *
+ * `time` is a timestamp calculated with a day-in-month value that may exceed
+ * the number of days in the month specified by `year` and `month`. If it does,
+ * it's clamped to be the first ms of the month above when rounding up, or the
+ * last ms in `month` when rounding down.
+ *
+ * Note that `month` is zero-indexed, i.e. Jan is 0.
+ * @return `time` unless clamped a described above.
+ */
+function clampImaginaryDays(
+  time: number,
+  year: number,
+  month: number,
+  round: "up" | "down",
+): number {
+  const nextMonth = Date.UTC(year, month + 1, 1);
+  if (time < nextMonth) return time;
+  return round === "up" ? nextMonth : nextMonth - 1;
 }
--- _datetime_test.ts
+++ _datetime_test.ts
@@ -1,60 +1,134 @@
-import { getUtcTimeSinceMidnight, Unit, utcDifference } from "./_datetime.ts";
+import {
+  differenceUtc,
+  monthDifferenceUtc,
+  timeSinceMidnightUtc,
+  Unit,
+} from "./_datetime.ts";
 import { date } from "./_testing.ts";
-import { assertEquals } from "./dev_deps.ts";
+import {
+  assert,
+  assertAlmostEquals,
+  assertEquals,
+  assertGreater,
+  assertLess,
+} from "./dev_deps.ts";
+import { assertUnreachable } from "./_misc.ts";
 
-Deno.test("getUTCTimeSinceMidnight()", () => {
+Deno.test("timeSinceMidnightUtc()", () => {
   const time = "13:14:15.678Z";
   const epoch = new Date(Date.parse(`1970-01-01T${time}`));
   const later = new Date(Date.parse(`2000-01-01T${time}`));
 
-  assertEquals(epoch.getTime(), getUtcTimeSinceMidnight(epoch));
-  assertEquals(epoch.getTime(), getUtcTimeSinceMidnight(later));
+  assertEquals(epoch.getTime(), timeSinceMidnightUtc(epoch));
+  assertEquals(epoch.getTime(), timeSinceMidnightUtc(later));
 });
 
-Deno.test("utcUnitDifference()", async (t) => {
-  const checks: Record<Unit, [string, string, number][]> = {
+interface Matcher<T> {
+  assertMatches(value: T): void;
+}
+
+class BoundaryCondition implements Matcher<number> {
+  constructor(
+    readonly target: number,
+    readonly direction: "lt" | "gt",
+    readonly tolerance: number,
+  ) {
+    if (tolerance <= 0) throw new Error("tolerance must be positive");
+  }
+
+  assertMatches(value: number) {
+    const distance = Math.abs(this.target - value);
+    switch (this.direction) {
+      case "lt": {
+        assertLess(value, this.target);
+        assertLess(distance, this.tolerance);
+        break;
+      }
+      case "gt": {
+        assertGreater(value, this.target);
+        assertLess(distance, this.tolerance);
+        break;
+      }
+      default:
+        assertUnreachable(this.direction);
+    }
+  }
+}
+
+function lt(target: number, distance: number = 0.01): BoundaryCondition {
+  return new BoundaryCondition(target, "lt", distance);
+}
+
+function gt(target: number, distance: number = 0.01): BoundaryCondition {
+  return new BoundaryCondition(target, "gt", distance);
+}
+
+class ApproximatelyEqualMatcher implements Matcher<number> {
+  constructor(readonly expected: number, readonly tolerance?: number) {}
+  assertMatches(value: number): void {
+    assertAlmostEquals(value, this.expected, this.tolerance);
+  }
+}
+
+function approx(
+  target: number,
+  tolerance: number | undefined = 0.01,
+): ApproximatelyEqualMatcher {
+  return new ApproximatelyEqualMatcher(target, tolerance);
+}
+
+Deno.test("differenceUtc()", async (t) => {
+  const checks: Record<Unit, [string, string, number | Matcher<number>][]> = {
     months: [
-      ["2001-02-03T04:05:06.000Z", "2001-02-03T04:05:06.000Z", 0],
-      // Same time next/prev year
-      ["2001-02-03T04:05:06.000Z", "2002-02-03T04:05:06.000Z", 12],
-      ["2001-02-03T04:05:06.000Z", "2000-02-03T04:05:06.000Z", -12],
-      // Same time next/prev day
-      ["2001-02-03T04:05:06.000Z", "2001-03-03T04:05:06.000Z", 1],
-      ["2001-02-03T04:05:06.000Z", "2001-01-03T04:05:06.000Z", -1],
-      // 1 calendar month, but to is 1ms before from time
-      ["2001-02-03T04:05:06.500Z", "2001-03-03T04:05:06.499Z", 0],
-      ["2001-02-03T04:05:06.500Z", "2001-01-03T04:05:06.501Z", 0],
-      // 1 calendar month, same time
-      ["2001-02-03T04:05:06.500Z", "2001-03-03T04:05:06.500Z", 1],
-      ["2001-02-03T04:05:06.500Z", "2001-01-03T04:05:06.500Z", -1],
-      // 14 calendar month, but to is 1ms before from time
-      ["2001-06-03T04:05:06.500Z", "2002-08-03T04:05:06.499Z", 13],
-      ["2001-06-03T04:05:06.500Z", "2000-04-03T04:05:06.501Z", -13],
-      // 14 calendar month, same time
-      ["2001-06-03T04:05:06.500Z", "2002-08-03T04:05:06.500Z", 14],
-      ["2001-06-03T04:05:06.500Z", "2000-04-03T04:05:06.500Z", -14],
+      // almost equal
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:05:06.499Z", lt(0)],
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:05:06.500Z", 0],
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:05:06.501Z", gt(0)],
+      // Partial month
+      ["2001-02-03T04:05:06.500Z", "2001-02-17T04:05:06.500Z", approx(0.5)],
+      ["2001-03-03T04:05:06.500Z", "2001-02-17T04:05:06.500Z", approx(-0.5)],
+      // several months, same year
+      ["2001-02-03T04:05:06.500Z", "2001-05-03T04:05:06.499Z", lt(3)],
+      ["2001-02-03T04:05:06.500Z", "2001-05-03T04:05:06.500Z", 3],
+      ["2001-02-03T04:05:06.500Z", "2001-05-03T04:05:06.501Z", gt(3)],
+      ["2001-05-03T04:05:06.500Z", "2001-02-03T04:05:06.499Z", lt(-3)],
+      ["2001-05-03T04:05:06.500Z", "2001-02-03T04:05:06.500Z", -3],
+      ["2001-05-03T04:05:06.500Z", "2001-02-03T04:05:06.501Z", gt(-3)],
+      // different year
+      ["2001-02-03T04:05:06.500Z", "2003-02-03T04:05:06.499Z", lt(24)],
+      ["2001-02-03T04:05:06.500Z", "2003-02-03T04:05:06.500Z", 24],
+      ["2001-02-03T04:05:06.500Z", "2003-02-03T04:05:06.501Z", gt(24)],
+      ["2002-02-03T04:05:06.500Z", "2000-02-03T04:05:06.499Z", lt(-24)],
+      ["2002-02-03T04:05:06.500Z", "2000-02-03T04:05:06.500Z", -24],
+      ["2002-02-03T04:05:06.500Z", "2000-02-03T04:05:06.501Z", gt(-24)],
       // from day not in to month
-      ["2001-01-31T04:05:06.500Z", "2001-03-01T04:05:06.500Z", 1],
-      ["2001-03-31T04:05:06.500Z", "2001-02-28T04:05:06.500Z", -1],
+      ["2001-01-31T04:05:06.500Z", "2001-03-01T00:00:00.000Z", gt(1)],
+      ["2001-01-31T04:05:06.500Z", "2001-02-28T23:59:59.999Z", lt(1)],
+      ["2001-03-31T04:05:06.500Z", "2001-03-01T00:00:00.000Z", gt(-1)],
+      ["2001-03-31T04:05:06.500Z", "2001-02-28T23:59:59.999Z", lt(-1)],
     ],
     quarters: [
       // same time
       ["2001-02-03T04:05:06.000Z", "2001-02-03T04:05:06.000Z", 0],
       // month before/after
-      ["2001-02-03T04:05:06.000Z", "2001-03-03T04:05:06.000Z", 0],
-      ["2001-02-03T04:05:06.000Z", "2001-01-03T04:05:06.000Z", 0],
+      ["2001-02-03T04:05:06.000Z", "2001-03-03T04:05:06.000Z", approx(1 / 3)],
+      ["2001-02-03T04:05:06.000Z", "2001-01-03T04:05:06.000Z", approx(-1 / 3)],
       // exactly 3 months before/after
       ["2001-02-03T04:05:06.000Z", "2001-05-03T04:05:06.000Z", 1],
       ["2001-02-03T04:05:06.000Z", "2000-11-03T04:05:06.000Z", -1],
       // 1ms off 3 months before/after
-      ["2001-02-03T04:05:06.500Z", "2001-05-03T04:05:06.499Z", 0],
-      ["2001-02-03T04:05:06.500Z", "2000-11-03T04:05:06.501Z", 0],
+      ["2001-02-03T04:05:06.500Z", "2001-05-03T04:05:06.501Z", gt(1)],
+      ["2001-02-03T04:05:06.500Z", "2001-05-03T04:05:06.499Z", lt(1)],
+      ["2001-02-03T04:05:06.500Z", "2000-11-03T04:05:06.501Z", gt(-1)],
+      ["2001-02-03T04:05:06.500Z", "2000-11-03T04:05:06.499Z", lt(-1)],
       // exactly 15 months before/after
       ["2001-02-03T04:05:06.000Z", "2002-05-03T04:05:06.000Z", 5],
       ["2001-02-03T04:05:06.000Z", "1999-11-03T04:05:06.000Z", -5],
       // 1ms off 15 months before/after
-      ["2001-02-03T04:05:06.500Z", "2002-05-03T04:05:06.499Z", 4],
-      ["2001-02-03T04:05:06.500Z", "1999-11-03T04:05:06.501Z", -4],
+      ["2001-02-03T04:05:06.500Z", "2002-05-03T04:05:06.501Z", gt(5)],
+      ["2001-02-03T04:05:06.500Z", "2002-05-03T04:05:06.499Z", lt(5)],
+      ["2001-02-03T04:05:06.500Z", "1999-11-03T04:05:06.501Z", gt(-5)],
+      ["2001-02-03T04:05:06.500Z", "1999-11-03T04:05:06.499Z", lt(-5)],
     ],
     years: [
       // same time
@@ -63,27 +137,53 @@ Deno.test("utcUnitDifference()", async (t) => {
       ["2001-02-03T04:05:06.000Z", "2002-02-03T04:05:06.000Z", 1],
       ["2001-02-03T04:05:06.000Z", "2000-02-03T04:05:06.000Z", -1],
       // 1ms off exactly 1 year before/after
-      ["2001-02-03T04:05:06.500Z", "2002-02-03T04:05:06.499Z", 0],
-      ["2001-02-03T04:05:06.500Z", "2000-02-03T04:05:06.501Z", 0],
+      ["2001-02-03T04:05:06.500Z", "2002-02-03T04:05:06.501Z", gt(1)],
+      ["2001-02-03T04:05:06.500Z", "2002-02-03T04:05:06.499Z", lt(1)],
+      ["2001-02-03T04:05:06.500Z", "2000-02-03T04:05:06.501Z", gt(-1)],
+      ["2001-02-03T04:05:06.500Z", "2000-02-03T04:05:06.499Z", lt(-1)],
     ],
 
     days: [
-      ["2001-02-03T04:05:06.500Z", "2001-02-04T04:05:06.499Z", 0],
-      ["2001-02-03T04:05:06.500Z", "2001-02-02T06:05:06.501Z", 0],
-      ["2001-02-03T04:05:06Z", "2001-02-05T04:05:06Z", 2],
-      ["2001-02-03T04:05:06Z", "2001-02-01T04:05:06Z", -2],
-    ],
-    hours: [
-      ["2001-02-03T04:05:06.500Z", "2001-02-03T05:05:06.499Z", 0],
-      ["2001-02-03T04:05:06.500Z", "2001-02-03T03:05:06.501Z", 0],
-      ["2001-02-03T04:05:06Z", "2001-02-03T06:05:06Z", 2],
-      ["2001-02-03T04:05:06Z", "2001-02-03T02:05:06Z", -2],
+      // 0 days
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:05:06.499Z", lt(0)],
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:05:06.500Z", 0],
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:05:06.501Z", gt(0)],
+      // 1 day
+      ["2001-02-03T04:05:06.500Z", "2001-02-04T04:05:06.499Z", lt(1)],
+      ["2001-02-03T04:05:06.500Z", "2001-02-04T04:05:06.500Z", 1],
+      ["2001-02-03T04:05:06.500Z", "2001-02-04T04:05:06.501Z", gt(1)],
+      // -1 day
+      ["2001-02-04T04:05:06.499Z", "2001-02-03T04:05:06.500Z", gt(-1)],
+      ["2001-02-04T04:05:06.500Z", "2001-02-03T04:05:06.500Z", -1],
+      ["2001-02-04T04:05:06.501Z", "2001-02-03T04:05:06.500Z", lt(-1)],
+      // lots
+      ["2001-02-03T04:05:06Z", "2002-02-05T04:05:06Z", 367],
+      ["2002-02-05T04:05:06Z", "2001-02-03T04:05:06Z", -367],
     ],
     weeks: [
-      ["2001-02-03T04:05:06.500Z", "2001-02-10T04:05:06.499Z", 0],
-      ["2001-02-10T04:05:06.500Z", "2001-02-03T04:05:06.501Z", 0],
-      ["2001-02-03T04:05:06Z", "2001-02-17T04:05:06Z", 2],
-      ["2001-02-17T04:05:06Z", "2001-02-03T04:05:06Z", -2],
+      ["2001-02-03T04:05:06.500Z", "2001-02-17T04:05:06.499Z", lt(2)],
+      ["2001-02-03T04:05:06.500Z", "2001-02-17T04:05:06.500Z", 2],
+      ["2001-02-03T04:05:06.500Z", "2001-02-17T04:05:06.501Z", gt(2)],
+    ],
+    hours: [
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T02:05:06.499Z", lt(-2)],
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T02:05:06.500Z", -2],
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T02:05:06.501Z", gt(-2)],
+    ],
+    minutes: [
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:03:06.499Z", lt(-2)],
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:03:06.500Z", -2],
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:03:06.501Z", gt(-2)],
+    ],
+    seconds: [
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:05:04.499Z", lt(-2)],
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:05:04.500Z", -2],
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:05:04.501Z", gt(-2)],
+    ],
+    milliseconds: [
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:05:06.499Z", -1],
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:05:06.500Z", 0],
+      ["2001-02-03T04:05:06.500Z", "2001-02-03T04:05:06.502Z", 2],
     ],
   };
 
@@ -93,13 +193,143 @@ Deno.test("utcUnitDifference()", async (t) => {
 
   for (const [unit, i, check] of params) {
     await t.step(`${unit}[${i}]`, () => {
-      const [from, to, count] = check;
-      const diff = utcDifference({
-        from: date(from),
-        to: date(to),
-        unit,
-      });
-      assertEquals(diff, count);
+      const [from, to, expected] = check;
+      const diff = differenceUtc(date(from), date(to), unit);
+      if (typeof expected === "number") assertEquals(diff, expected);
+      else expected.assertMatches(diff);
     });
   }
+});
+
+Deno.test("monthDifferenceUtc()", async (t) => {
+  await t.step("positive 1 month imaginary boundary", () => {
+    const from = date("2024-01-31T12:00:00Z");
+    const distBefore = monthDifferenceUtc(
+      from,
+      date("2024-02-29T23:59:59.999Z"),
+    );
+    assert(distBefore > 0.99);
+    assert(distBefore < 1);
+
+    const distOn = monthDifferenceUtc(
+      from,
+      date("2024-03-01T00:00:00Z"),
+    );
+    assert(distOn > 1);
+    assert(distOn < 1.01);
+
+    const distAfter = monthDifferenceUtc(
+      from,
+      date("2024-03-01T00:00:00.001Z"),
+    );
+    assert(distAfter > 1);
+    assert(distAfter < 1.01);
+  });
+
+  await t.step("negative 1 month imaginary boundary", () => {
+    const from = date("2024-03-31T12:00:00Z");
+
+    const distBefore = monthDifferenceUtc(
+      from,
+      date("2024-03-01T00:00:00Z"),
+    );
+    assert(distBefore < -0.99);
+    assert(distBefore > -1);
+
+    const distOn = monthDifferenceUtc(
+      from,
+      date("2024-02-29T23:59:59.999Z"),
+    );
+    // Maybe this should be < -1, but then there wouldn't be a reverse mapping
+    // for -1 to a ms value.
+    // assertEquals(distOn, -1.0);
+    // Actually let's make this < -1.0. This seems more natural, the boundary
+    // can be made the first ms of march, which is outside feb, which makes sense
+    assert(distOn < -1);
+    assert(distOn > -1.01);
+
+    const distAfter = monthDifferenceUtc(
+      from,
+      date("2024-02-29T23:59:59.998Z"),
+    );
+    assert(distAfter < -1);
+    assert(distAfter > -1.01);
+
+    assert(distAfter < distOn);
+  });
+
+  await t.step("positive 1 month boundary ", () => {
+    const from = date("2024-01-12T12:00:00.500Z");
+
+    const distBefore = monthDifferenceUtc(
+      from,
+      date("2024-02-12T12:00:00.499Z"),
+    );
+    assert(distBefore > 0.99);
+    assert(distBefore < 1);
+
+    const distOn = monthDifferenceUtc(
+      from,
+      date("2024-02-12T12:00:00.500Z"),
+    );
+    assertEquals(distOn, 1);
+
+    const distAfter = monthDifferenceUtc(
+      from,
+      date("2024-02-12T12:00:00.501Z"),
+    );
+    assert(distAfter > 1);
+    assert(distAfter < 1.01);
+  });
+
+  await t.step("negative 1 month boundary ", () => {
+    const from = date("2024-02-12T12:00:00.500Z");
+
+    const distBefore = monthDifferenceUtc(
+      from,
+      date("2024-01-12T12:00:00.501Z"),
+    );
+    assert(distBefore < -0.99);
+    assert(distBefore > -1);
+
+    const distOn = monthDifferenceUtc(
+      from,
+      date("2024-01-12T12:00:00.500Z"),
+    );
+    assertEquals(distOn, -1);
+
+    const distAfter = monthDifferenceUtc(
+      from,
+      date("2024-01-12T12:00:00.499Z"),
+    );
+    assert(distAfter < -1);
+    assert(distAfter > -1.01);
+  });
+
+  await t.step("clamped upper", () => {
+    const from = date("2024-01-31T12:00:00.500Z");
+
+    // The lower bound will be 2024-03-31T12:00:00.501Z
+    // The upper bound will be 2024-04-31T12:00:00.501Z which is out of range,
+    // and thus will be clamped to 2024-05-01T00:00:00Z
+    const distAtStart = monthDifferenceUtc(
+      from,
+      date("2024-03-31T12:00:00.500Z"),
+    );
+    assertEquals(distAtStart, 2);
+
+    const distAfterStart = monthDifferenceUtc(
+      from,
+      date("2024-03-31T12:00:00.501Z"),
+    );
+    assert(distAfterStart > 2);
+    assert(distAfterStart < 2.01);
+
+    const distBeforeEnd = monthDifferenceUtc(
+      from,
+      date("2024-04-30T23:59:59.999Z"),
+    );
+    assert(distBeforeEnd < 3);
+    assert(distBeforeEnd > 2.99);
+  });
 });
