18d7830aab3dd5338a0f6929f1fd0c5562a9c674
Hal Blackburn <hwtb2@cam.ac.uk>
2024-05-06 09:13:53 +0000

    chore: add dev deps for _datetime_test.ts

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,14 +1,21 @@
 export {
   assert,
+  assertAlmostEquals,
   assertEquals,
+  assertGreater,
   assertInstanceOf,
+  assertLess,
   assertNotEquals,
   assertRejects,
   assertStringIncludes,
 } from "https://deno.land/std@0.221.0/assert/mod.ts";
+
 export { assertSnapshot } from "https://deno.land/std@0.221.0/testing/snapshot.ts";
+
 export {
   assertSpyCall,
   spy,
   stub,
 } from "https://deno.land/std@0.221.0/testing/mock.ts";
+
+export { FakeTime } from "https://deno.land/std@0.221.0/testing/time.ts";
